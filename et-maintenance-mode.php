a869f70229d4954edc0edef5536c200dadfdf916
Gavin Rohrer <gavin@rohrer.org>
2022-04-20 11:52:41 -0400

    basic plugin info for eh-52

--- et-maintenance-mode.php
+++ et-maintenance-mode.php
@@ -1,4 +1,10 @@
 <?php
 /**
  * Plugin Name: -ET-Maintenance-Mode
+ * Plugin URI: https://github.com/gfaster/et-maintenance-mode
+ * Description: A WordPress plugin to facilitate slow updates
+ * Version: 0.1
+ * Requires at least: 5.9
+ * Requires PHP: 7.4
+ * Author: Gavin Rohrer, JJ Rohrer
  */
